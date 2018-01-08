cfb053f8acf3e1b1233beca9daa470c2d3679049
Rubi1gupt <nishithk@microsoft.com>
2018-01-07 20:52:39 -0800

    Updated app with more pages

--- HelloWorld/app/src/main/java/com/example/rubig/helloworld/DetailsActivity.java
+++ HelloWorld/app/src/main/java/com/example/rubig/helloworld/DetailsActivity.java
@@ -5,17 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.rubig.helloworld.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details);
     }
-    public void sendMessage(View view) {
+
+    public void backHomeMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void backRestaurantsMessage(View view) {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
-
 }
